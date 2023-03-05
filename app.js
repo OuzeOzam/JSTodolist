89530c233e1320da17fff123dc468647406c473f
Ouze <ossama.benjemaa@gmail.com>
2023-03-05 12:30:14 +0100

    Add li check to todolist

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //SELECTEURS
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
-const todoLust = document.querySelector(".todo-list");
+const todoList = document.querySelector(".todo-list");
 
 //ECOUTEURS
 todoButton.addEventListener("click", addTodo);
@@ -10,6 +10,22 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
   event.preventDefault();
   //Todo DIV
-  const todoDiv = document.createElement
-  console.log("Hello");
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Creer le Li
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //Bouton Check
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Bouton Supprimer
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //Ajouter notre todo à la todolist
+  todoList.appendChild(todoDiv);
 }
