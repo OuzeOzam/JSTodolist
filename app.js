b19b7741f99bc3d9d7f8d14e2e6cdac8a86b1f2b
Ouze <ossama.benjemaa@gmail.com>
2023-03-05 12:20:54 +0100

    Body Init and Styling

---
+++ app.js
@@ -0,0 +1,15 @@
+//SELECTEURS
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoLust = document.querySelector(".todo-list");
+
+//ECOUTEURS
+todoButton.addEventListener("click", addTodo);
+
+//FONCTIONS
+function addTodo(event) {
+  event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement
+  console.log("Hello");
+}
